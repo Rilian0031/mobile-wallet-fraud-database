865d31f7099b026c26bf8787996df39cd1a4f52f
lilian <rilian0031@gmail.com>
2024-09-16 14:11:55 +0000

    made changes to the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import Button from './button';
 import { AlignJustify } from 'lucide-react';
 
@@ -8,26 +7,10 @@ const Navbar = () => {
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
-
-
-import { AlignJustify } from 'lucide-react';
-import FraudReportModal from './reportFraudModal/reportFraudModal'; 
-
-const Navbar = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-
   };
 
   return (
     <div className="flex items-center justify-between p-4">
-
       <p className="text-blue-700">Mobile Wallet Fraud Database</p>
       <div className="relative">
         <button
@@ -52,18 +35,6 @@ const Navbar = () => {
           </div>
         )}
       </div>
-
-      <p className='text-blue-700'>Mobile Wallet Fraud Database</p>
-      <div className='flex gap-4'>
-        <Button variant="solid" onClick={handleOpenModal}>Report fraudulent number</Button>
-        <div className='flex items-center gap-2 py-2 px-2 border rounded-lg'>
-          <AlignJustify />
-          <p>MK</p>
-        </div>
-      </div>
-
-      <FraudReportModal isOpen={isModalOpen} onClose={handleCloseModal} />
-
     </div>
   );
 };
